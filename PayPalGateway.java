b65547c74d79b1e34f80abcaae05a7ca08c5fd57
MAUSHMI2411 <maushmihilda24@gmail.com>
2026-06-23 19:11:25 +0530

    Delete PayPalGateway.java

--- PayPalGateway.java
+++
@@ -1,9 +0,0 @@
-/**
- * Concrete component: the base notification channel (Email).
- */
-public class EmailNotifier implements Notifier {
-    @Override
-    public void send(String message) {
-        System.out.println("Email sent: " + message);
-    }
-}
