50f4e3539ad5edf12b257e3b627243d0634ba1d8
MAUSHMI2411 <maushmihilda24@gmail.com>
2026-06-23 19:06:56 +0530

    Delete Light.java

--- Light.java
+++
@@ -1,9 +0,0 @@
-/**
- * Exercise 4: Implementing the Adapter Pattern
- *
- * Target interface that the client code expects to use, regardless of
- * which payment gateway is plugged in behind the scenes.
- */
-public interface PaymentProcessor {
-    void processPayment(double amount);
-}
