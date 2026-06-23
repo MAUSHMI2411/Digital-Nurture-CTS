fc39b993dffc18f138da401e10e1bbf3c4f1549c
MAUSHMI2411 <maushmihilda24@gmail.com>
2026-06-23 19:11:11 +0530

    Delete ProxyImage.java

--- ProxyImage.java
+++
@@ -1,9 +0,0 @@
-/**
- * Adaptee #2: another third-party gateway, with a different signature
- * (amount in cents instead of dollars).
- */
-public class StripeGateway {
-    public void charge(long amountInCents) {
-        System.out.println("Stripe: charged " + amountInCents + " cents");
-    }
-}
