9996c8310a0f4c9f1a16621af0f4445bc309cd65
MAUSHMI2411 <maushmihilda24@gmail.com>
2026-06-23 19:07:41 +0530

    Delete StockMarket.java

--- StockMarket.java
+++
@@ -1,9 +0,0 @@
-/**
- * Exercise 2: Implementing the Factory Method Pattern
- *
- * Common interface for all document types the system can create.
- */
-public interface Document {
-    void open();
-    void save();
-}
