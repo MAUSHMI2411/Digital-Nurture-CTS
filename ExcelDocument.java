82ab9987496aa1b8c830c8158c9720ff80654e9d
MAUSHMI2411 <maushmihilda24@gmail.com>
2026-06-23 19:05:14 +0530

    Delete ExcelDocument.java

--- ExcelDocument.java
+++
@@ -1,8 +0,0 @@
-/**
- * Exercise 8: Implementing the Strategy Pattern
- *
- * Strategy interface for interchangeable payment algorithms.
- */
-public interface PaymentStrategy {
-    void pay(double amount);
-}
