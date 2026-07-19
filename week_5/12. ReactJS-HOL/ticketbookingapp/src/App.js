74997b4c6d5b027cd0d4bbbf3351d4dba3cd6c48
MAUSHMI2411 <maushmihilda24@gmail.com>
2026-07-19 11:46:01 +0530

    Add files via upload

---
+++ week_5/12. ReactJS-HOL/ticketbookingapp/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import LoginControl from "./LoginControl";
+
+function App() {
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        marginTop: "100px"
+      }}
+    >
+      <LoginControl />
+    </div>
+  );
+}
+
+export default App;
---
+++ week_5/13. ReactJS-HOL/bloggerapp/src/BlogDetails.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { blogs } from "./data";
+
+function BlogDetails() {
+
+  return (
+    <div>
+      <h2>Blog Details</h2>
+
+      {blogs.map((blog) => (
+        <div key={blog.id}>
+          <h3>{blog.title}</h3>
+          <p>{blog.author}</p>
+        </div>
+      ))}
+
+    </div>
+  );
+}
+
+export default BlogDetails;
